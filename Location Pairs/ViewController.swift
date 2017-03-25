cfedbe36cfe5ce6fb392c0646f5079043c4b2b01
Joshua Areogun <jareogun@gmail.com>
2017-03-25 12:12:37 +0000

    Initial Commit

---
+++ Location Pairs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Location Pairs
+//
+//  Created by Joshua Areogun on 25/03/2017.
+//  Copyright © 2017 Joshua Areogun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
