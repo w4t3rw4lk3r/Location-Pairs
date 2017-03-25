67094ec40eba9538bd7089d5607ffb429b5157d1
Joshua Areogun <jareogun@gmail.com>
2017-03-25 13:31:49 +0000

    - Added nibs for the sample leaderboard cell & location cell. - Added layout constraints. - Basic UI Complete; Polish to follow

---
+++ Location Pairs/LeaderboardCellView.swift
@@ -0,0 +1,9 @@
+//
+//  LeaderboardCellView.swift
+//  Location Pairs
+//
+//  Created by Joshua Areogun on 25/03/2017.
+//  Copyright © 2017 Joshua Areogun. All rights reserved.
+//
+
+import Foundation
---
+++ Location Pairs/LocationCardView.swift
@@ -0,0 +1,9 @@
+//
+//  LocationCardView.swift
+//  Location Pairs
+//
+//  Created by Joshua Areogun on 25/03/2017.
+//  Copyright © 2017 Joshua Areogun. All rights reserved.
+//
+
+import Foundation
