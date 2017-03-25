b46841fb2db5ed76d8d302188ad1201d96b6b167
Joshua Areogun <jareogun@gmail.com>
2017-03-25 13:12:25 +0000

    - Quick cleanup

---
+++ Location Pairs/GlobalConstants.swift
@@ -0,0 +1,16 @@
+//
+//  GlobalConstants.swift
+//  Location Pairs
+//
+//  Created by Joshua Areogun on 25/03/2017.
+//  Copyright © 2017 Joshua Areogun. All rights reserved.
+//
+
+import UIKit
+
+struct GlobalConstants {
+
+    static let kNumberOfRestaurants:Int = 8
+    static let kNumberOfMilesFromLocation:CGFloat = 1.0
+    
+}
